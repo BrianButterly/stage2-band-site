132fb3b4d48bdd5d01c0717657e8bd5314364dcb
Brian Butterly <butterlybrian@gmail.com>
2021-10-25 15:26:43 -0500

    scroll arrow with animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Fade from "react-reveal/Fade";
+import { Link } from "react-scroll";
 
 function Home() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <div className="home" id="home">
       <Fade bottom></Fade>
+      <img src="logo.png" alt="logo" class="stage2" />
+      <Link
+        activeClass="active"
+        to="about"
+        spy={true}
+        smooth={true}
+        offset={-55}
+        duration={150}
+        onClick={() =>
+          setTimeout(() => {
+            setExpanded(false);
+          }, 150)
+        }
+      >
+        <svg class="arrows">
+          <path class="a1" d="M0 0 L30 32 L60 0"></path>
+          <path class="a2" d="M0 20 L30 52 L60 20"></path>
+          <path class="a3" d="M0 40 L30 72 L60 40"></path>
+        </svg>
+      </Link>
     </div>
   );
 }
