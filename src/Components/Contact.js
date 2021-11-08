f8399ad5f46568ff0a282d83d5dff1a566856b79
Brian Butterly <butterlybrian@gmail.com>
2021-11-08 13:55:06 -0600

    ignore

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+
 
 function Contact() {
-  return <div className="contact" id="contact">
-    
-  </div>;
+  
+
+  return (
+    <div className="contact" id="contact">
+      <Fade bottom></Fade>
+      
+    </div>
+  );
 }
 
 export default Contact;
