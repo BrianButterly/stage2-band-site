58c3d1f249248600b0ad985fe3d089cd96030460
Brian Butterly <butterlybrian@gmail.com>
2022-01-07 12:14:54 -0600

    changes

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -7,40 +7,7 @@ function AboutUs() {
       <Fade bottom>
         <br />
         <span>
-          Consectetur magna fugiat pariatur aliquip dolore amet minim magna quis
-          sint mollit officia velit. Exercitation officia ut esse culpa
-          consectetur cupidatat proident ut anim aliquip adipisicing. Ipsum in
-          ex mollit anim esse dolore id. Ipsum elit cupidatat dolore et id do
-          exercitation excepteur. Veniam sint ex excepteur est ullamco
-          adipisicing. Fugiat sint aliqua deserunt dolor cupidatat eu nulla
-          voluptate fugiat dolor proident voluptate. Occaecat voluptate
-          cupidatat proident ipsum ea velit nostrud irure proident incididunt.
-          Commodo aliqua tempor aliquip pariatur. Labore deserunt et dolore
-          irure. Do cillum voluptate amet ea elit occaecat quis ea proident non
-          quis. Et ad elit ad nostrud aliquip reprehenderit aliquip laboris
-          nulla. Consectetur ea irure nulla sit ea cillum sint velit. Est in
-          tempor exercitation elit velit quis nostrud. Laboris cupidatat magna
-          veniam eu duis officia veniam veniam sit quis adipisicing non sunt.
-          Laboris est amet labore enim exercitation dolore consequat ad minim
-          eiusmod cupidatat. Excepteur exercitation est deserunt pariatur
-          occaecat fugiat voluptate. Lorem culpa veniam magna non elit nisi
-          incididunt adipisicing nostrud sint enim in laborum consequat. Dolor
-          esse aliquip adipisicing veniam commodo laboris duis exercitation aute
-          labore. Veniam incididunt Lorem labore incididunt nostrud consectetur
-          eu in id minim dolor eu sunt. Magna cupidatat ea id ex et. Officia
-          dolore culpa irure sit ex duis ea fugiat cillum. In enim quis culpa
-          excepteur ut aute et ex id. Occaecat anim non cillum cillum minim
-          reprehenderit fugiat est esse minim occaecat duis. Consectetur minim
-          voluptate non eiusmod commodo aute labore. Elit deserunt irure ipsum
-          labore. Eiusmod sit incididunt pariatur deserunt commodo deserunt
-          deserunt excepteur mollit id veniam consequat ea. Consequat dolore et
-          dolore consequat eu nulla eiusmod elit. Magna mollit aute aliquip
-          proident laboris ex nulla. Eu nulla aute proident consectetur sunt
-          officia elit. Sint nostrud culpa irure dolor id non officia cillum.
-          Ipsum sit consectetur nisi ea. In irure in velit cupidatat tempor
-          cillum irure deserunt nostrud et laboris in. Sunt ex commodo deserunt
-          id esse sit occaecat Lorem enim. Deserunt est id laboris sit Lorem
-          laborum incididunt.
+          
         </span>
         <img src="img3.jpeg" alt="logo" className="band" />
       </Fade>
